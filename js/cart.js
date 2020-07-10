41a2c31bbee65138632818183bc4279b4df017ef
hecathe <63404814+hecathe@users.noreply.github.com>
2020-07-10 12:05:39 +0300

    Защита

--- js/cart.js
+++ js/cart.js
@@ -1,23 +1,33 @@
 var buyLink = document.querySelectorAll(".buy-btn");
 var cartPopup = document.querySelector(".pop-up-cart");
 var cartClose = cartPopup.querySelector(".pop-up-close");
+var contShopping = cartPopup.querySelector(".btn-continue-shopping");
 
-buyLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add("pop-up-show");
-});
+for(var i = 0; i <= buyLink.length; i++) {
+  buyLink[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("pop-up-show");
+  });
+  cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("pop-up-show");
+  });
 
-cartClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("pop-up-show");
-});
+  contShopping.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("pop-up-show");
+  })
 
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    if (cartPopup.classList.contains("pop-up-show")) {
-      evt.preventDefault();
-      cartPopup.classList.remove("pop-up-show");
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      if (cartPopup.classList.contains("pop-up-show")) {
+        evt.preventDefault();
+        cartPopup.classList.remove("pop-up-show");
+      }
     }
-  }
-});
+  });
+}
+
+
+
 
